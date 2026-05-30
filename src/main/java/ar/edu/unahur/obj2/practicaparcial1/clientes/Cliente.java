ca28429bfcdb9d15c00595761ff531dd7bcf19e1
gabrielledezma21 <gabrielagustin.ledezma@estudiantes.unahur.edu.ar>
2026-05-30 14:10:01 -0300

    test de excepciones

--- src/main/java/ar/edu/unahur/obj2/practicaparcial1/clientes/Cliente.java
+++ src/main/java/ar/edu/unahur/obj2/practicaparcial1/clientes/Cliente.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ar.edu.unahur.obj2.practicaparcial1.clientes.criterios.Criterio;
+import ar.edu.unahur.obj2.practicaparcial1.excepciones.DniErroneoException;
 import ar.edu.unahur.obj2.practicaparcial1.recetas.Receta;
 
 public class Cliente {
@@ -12,8 +13,15 @@ public class Cliente {
     private List<Receta> recetasRecibidas = new ArrayList<>();
 
     public Cliente(Integer DNI, Criterio criterio) {
-        this.DNI = DNI;
+        this.DNI = validarDni(DNI);
         this.criterio = criterio;
+    }
+
+    private Integer validarDni(Integer dni){
+        if (dni < 1000) {
+            throw new DniErroneoException();
+        }
+        return dni;
     }
 
     public Integer getDNI() {
--- src/test/java/ar/edu/unahur/obj2/practicaparcial1/clientes/ClienteTest.java
+++ src/test/java/ar/edu/unahur/obj2/practicaparcial1/clientes/ClienteTest.java
@@ -1,11 +1,18 @@
 package ar.edu.unahur.obj2.practicaparcial1.clientes;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import ar.edu.unahur.obj2.practicaparcial1.clientes.criterios.AltoValorNutricional;
 import ar.edu.unahur.obj2.practicaparcial1.clientes.criterios.ComeTutti;
 import ar.edu.unahur.obj2.practicaparcial1.clientes.criterios.Criterio;
+import ar.edu.unahur.obj2.practicaparcial1.excepciones.DniErroneoException;
+import ar.edu.unahur.obj2.practicaparcial1.recetas.Receta;
+import ar.edu.unahur.obj2.practicaparcial1.recetas.RecetaBase;
 
 public class ClienteTest {
 
@@ -17,6 +24,29 @@ public class ClienteTest {
         Cliente c = new Cliente(1234, criterio);
 
         assertEquals(criterio, c.getCriterio());
+
+    }
+
+    @Test
+    @DisplayName("Probar si le gusta una receta basica")
+    void testLeGustaEstaReceta() {
+        Criterio criterio = new AltoValorNutricional(150);
+        Cliente c = new Cliente(1234, criterio);
+        Receta unaReceta = new RecetaBase(2, "Pepe", "fideos con manteca", 200);
+        Receta otraReceta = new RecetaBase(1, "Pepe", "fideos con queso", 100);
+
+        assertTrue(c.leGustaEstaReceta(unaReceta));
+        assertFalse(c.leGustaEstaReceta(otraReceta));
+    }
+
+    @Test
+    @DisplayName("Probar excepcion de dni")
+    void validarDni() {
+
+        assertThrows(DniErroneoException.class, () -> {
+            Cliente c = new Cliente(123, new ComeTutti());
+        }
+    );
 
     }
 }
